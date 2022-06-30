52d4116980fafd4e0cc3a4b46c178cccc9693197
Mila Kostic <milakostic.eng@gmail.com>
2022-06-30 11:30:54 +0200

    if else in app listen

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const app = express();
 
-app.listen(4000, (err) => {
+app.listen(4000, err => {
+    if (err) {
+    console.log(err);
+    }
+    else {
   console.log('Server is runing on port: 4000');
+    }
 });
