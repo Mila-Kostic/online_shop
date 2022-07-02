77da4bcd87a92e971aa7b5d2255ec52d867d98a9
Mila Kostic <milakostic.eng@gmail.com>
2022-07-02 21:16:19 +0200

    req.body console  with postman

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser'); 
+
 
 const app = express();
+//const router = express.Router(); // router is depreceted, now is change it in line 13 also, we need app.post no router.post
 
+// for parsing machine code in json 
+app.use(bodyParser.urlencoded({ extended: false}));
+// parse app/json
+app.use(bodyParser.json());
+
+app.post('/api/login', (req, res) => {
+  console.log(req.body);
+  //req is requst from frontend, res is response from backend
+  res.send('login API call is working.')
+});
+
+
+
+
+//call back function-last function in program
 app.listen(4000, err => {
     if (err) {
     console.log(err);
@@ -10,3 +28,7 @@ app.listen(4000, err => {
   console.log('Server is runing on port: 4000');
     }
 });
+
+
+
+//mongodb+srv://Mila:<password>@cluster0.sc5vi.mongodb.net/?retryWrites=true&w=majority   
